107b0dcb9d4cd2e66937eebc4e379822c2151271
xJasonShane <jenson_shan@yeah.net>
2025-06-06 21:46:50 +0800

    feat: 添加新练习文件

---
+++ BOOK Python编程：从入门到实践（第2版）/Chapter2 变量和简单数据类型/250606 02 simpleMessage Practice2-1.py
@@ -0,0 +1,3 @@
+# Q:将一条消息赋给变量，并将其打印出来。
+message = "This is a simple message."
+print(message)
---
+++ BOOK Python编程：从入门到实践（第2版）/Chapter2 变量和简单数据类型/250606 03 simpleMessages Practice2-2.py
@@ -0,0 +1,6 @@
+# Q:将一条消息赋给变量，并将其打印出来；再将变量的值修改为一条新消息，并将其打印出来。
+message = "Hello, World!"
+print(message)
+
+message = "This is a new message."
+print(message)
